28c3860c95679f2fe694d141013c0b1914966831
danbaulk <danielbaulk@gmail.com>
2024-12-04 07:17:23 +0000

    update dump file

--- fpl-sql-dump.sql
+++ fpl-sql-dump.sql
@@ -18,6 +18,118 @@ USE `fpl`;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `fixtures`
+--
+
+DROP TABLE IF EXISTS `fixtures`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `fixtures` (
+  `id` int(11) NOT NULL,
+  `gameweek` int(11) DEFAULT NULL,
+  `home_team` int(11) DEFAULT NULL,
+  `away_team` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_idx` (`gameweek`),
+  KEY `home_team_idx` (`home_team`),
+  KEY `away_team_idx` (`away_team`),
+  CONSTRAINT `away_team` FOREIGN KEY (`away_team`) REFERENCES `teams` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `gameweek` FOREIGN KEY (`gameweek`) REFERENCES `gameweeks` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `home_team` FOREIGN KEY (`home_team`) REFERENCES `teams` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `gameweeks`
+--
+
+DROP TABLE IF EXISTS `gameweeks`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `gameweeks` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `is_next` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `players`
+--
+
+DROP TABLE IF EXISTS `players`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `players` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `team` int(11) DEFAULT NULL,
+  `position` int(11) DEFAULT NULL,
+  `availability` varchar(45) DEFAULT NULL,
+  `price` int(11) DEFAULT NULL,
+  `form` varchar(45) DEFAULT NULL,
+  `influence` varchar(45) DEFAULT NULL,
+  `creativity` varchar(45) DEFAULT NULL,
+  `threat` varchar(45) DEFAULT NULL,
+  `ict_index` varchar(45) DEFAULT NULL,
+  `xG` varchar(45) DEFAULT NULL,
+  `xA` varchar(45) DEFAULT NULL,
+  `xGI` varchar(45) DEFAULT NULL,
+  `xGC` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `team_idx` (`team`),
+  CONSTRAINT `team` FOREIGN KEY (`team`) REFERENCES `teams` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `predictions`
+--
+
+DROP TABLE IF EXISTS `predictions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `predictions` (
+  `player` int(11) NOT NULL,
+  `gameweek` int(11) NOT NULL,
+  `fixture` int(11) NOT NULL,
+  `prediction` varchar(45) DEFAULT NULL,
+  `confidence_blank` varchar(45) DEFAULT NULL,
+  `confidence_return` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`player`,`gameweek`,`fixture`),
+  KEY `player_idx` (`player`),
+  KEY `gameweek_idx` (`gameweek`),
+  KEY `fixture_idx` (`fixture`),
+  CONSTRAINT `fixtureid` FOREIGN KEY (`fixture`) REFERENCES `fixtures` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `gameweekid` FOREIGN KEY (`gameweek`) REFERENCES `gameweeks` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `playerid` FOREIGN KEY (`player`) REFERENCES `players` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `teams`
+--
+
+DROP TABLE IF EXISTS `teams`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `teams` (
+  `id` int(11) NOT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `form` varchar(45) DEFAULT NULL,
+  `strength` int(11) DEFAULT NULL,
+  `overall_strength_home` int(11) DEFAULT NULL,
+  `overall_strength_away` int(11) DEFAULT NULL,
+  `attack_strength_home` int(11) DEFAULT NULL,
+  `attack_strength_away` int(11) DEFAULT NULL,
+  `defence_strength_home` int(11) DEFAULT NULL,
+  `defence_strength_away` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping routines for database 'fpl'
 --
 /*!50003 DROP PROCEDURE IF EXISTS `getFixtures` */;
@@ -62,6 +174,30 @@ BEGIN
 SELECT p.id, p.name, p.team, p.form, p.influence, p.creativity, p.threat, p.ict_index, p.xG, p.xA, p.xGI, p.xGC
 FROM fpl.players AS p
 WHERE p.availability = 'a' AND p.position = requested_position;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `getPredictions` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `getPredictions`(
+    IN requested_gameweek int
+)
+BEGIN
+SELECT p.id, p.name, p.team, p.position, p.price, pr.confidence_return FROM predictions AS pr
+JOIN players AS p ON pr.player = p.id
+WHERE pr.gameweek = requested_gameweek
+ORDER BY pr.confidence_return DESC;
 END ;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -264,4 +400,4 @@ DELIMITER ;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-10-20 15:49:09
+-- Dump completed on 2024-12-04  7:12:20
